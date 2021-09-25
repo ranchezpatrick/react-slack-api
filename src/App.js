f62904dfc4874a89c02e4b2a6574b715963f33f0
Richelle Lobo <lobo.r0018@gmail.com>
2021-09-25 09:37:42 +0800

    Add Temporary Main UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import RegistraionForm from './components/RegistrationForm';
+import {BrowserRouter as Link} from "react-router-dom"
 import './App.css';
-
-const inputData = {
-  name: 'Francis'
-}
-
 function App() {
+
+  const Sidebar = () => {
+    return (
+      <div id="mySidebar" className="sidebar">
+        <ul>
+          <li className="closebtn" onClick={CloseNav}>
+            <Link to="/">x</Link>
+          </li>
+          <li>
+            <Link to="/login">Channels</Link>
+          </li>
+          <li>
+            <Link to="/registration">Direct Message</Link>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+
+  const Main = () => {
+    return (
+      <div id="main">
+        <button className="openbtn" onClick={OpenNav}>☰</button>
+      </div>
+    )
+  }
+  
+  const OpenNav = () => {
+    document.getElementById("mySidebar").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+  }
+  
+  const CloseNav = () => {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
   return (
     <div className="App">
-      <RegistraionForm name={inputData.name} />
+      <Sidebar />
+      <Main />
     </div>
   );
 }
